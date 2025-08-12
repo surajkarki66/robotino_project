05056a0f4084b928ff2cec6a46fba504ef824e69
SurajKarki <suraj.karki500@protonmail.com>
2025-08-13 00:31:13 +0200

    add waypoints

--- src/nav/PathPlanningForRobotino/PathPlanningRRT.m
+++ src/nav/PathPlanningForRobotino/PathPlanningRRT.m
@@ -1,70 +1,32 @@
-% Plan Mobile Robot Paths Using RRT
+%% ============================
+%  Dubins Path with Nodes & Stations
+% ============================
+clear; clc; close all;
 
-% Load Occupancy Map
+% --- Load Occupancy Map ---
 load('IOTFactoryOccupancyGridMap.mat', 'map');
 map.GridLocationInWorld = [-39.975, -39.975];
-figure;
-show(map);
-hold on;
 
+% --- Define Nodes ---
 coords_nodes = [
--15.2975,  1.70325
--15.2618,  0.852601
--14.8237,  5.20321
--14.7836,  2.71579
--14.2751,  3.92515
--14.2355,  1.66057
--14.215,   0.83
--14.0958,  2.77557
--13.1368,  2.67684
--13.1223,  1.82118
--13.0778,  1.03293
--11.942,   2.14404
--11.9117,  1.22906
--10.7464,  2.74653
--10.7194,  1.88722
--10.6823,  1.01414
--9.37005,  2.18178
--9.28013,  1.26069
--8.67676, -0.987635
--8.52921,  1.95023
--8.46591,  1.24895
--8.33197,  2.72984
--7.81392, -0.855324
--7.73612, -1.71582
--7.64132, -3.42178
--7.64,     1.329
--7.17804, -0.273202
--6.83323, -1.7505
--5.66786, -0.098643
--5.616,   -0.951
--5.5545,  -3.00751
--5.53319, -3.77273
--4.33607, -1.82372
--3.71488, -0.179186
--3.65801, -1.02993
--3.649,    0.764
--3.60114, -1.88067
--3.53667, -3.57462
--2.59693, -0.292615
--2.59474,  0.314477
--2.50361, -0.990156
--2.47543, -3.5765
--2.46034, -1.87816
--1.87,     0.871
--1.83542, -0.0391468
--1.747,   -1.418
--1.39255, -0.818823
--1.10676,  1.37889
--1.07194,  0.101598
--0.50355, -0.949287
--0.276,    0.067
--0.161829, 1.52521
+-15.2618,  0.852601 %12
+-14.215,   0.83 %11
+-13.1368,  2.67684 %1
+-10.7464,  2.74653 %2
+-8.33197,  2.72984 %3
+-7.64,     1.329 %4
+-6.83323, -1.7505 %7
+-5.616,   -0.951 %10
+-5.5545,  -3.00751 %8
+-4.33607, -1.82372 %9
+-2.59474,  0.314477 %5
+-1.747,   -1.418 %6
+-0.50355, -0.949287 %15
+-0.276,    0.067 %13
+-0.161829, 1.52521 %14
 ];
 
-plot(coords_nodes(:,1), coords_nodes(:,2), 'ro', 'MarkerSize', 6, 'LineWidth', 1.5);
-
-
+% --- Define Stations ---
 coords_stations = [
 -15.39,   2.552
 -15.752,  5.251
@@ -80,74 +42,77 @@ coords_stations = [
 -12.313,  4.249
 ];
 
-plot(coords_stations(:,1), coords_stations(:,2), 'go', 'MarkerSize', 6, 'LineWidth', 1.5);
+% --- First Figure: Map with nodes & stations ---
+figure;
+show(map);
+hold on;
+plot(coords_nodes(:,1), coords_nodes(:,2), 'go', ...
+    'MarkerSize', 6, 'LineWidth', 1.5, 'MarkerFaceColor', 'g');
+plot(coords_stations(:,1), coords_stations(:,2), 'yo', ...
+    'MarkerSize', 6, 'LineWidth', 1.5, 'MarkerFaceColor', 'y');
+legend('Nodes (Green)', 'Stations (Yellow)');
 
-hold off;
-
-
-% Set start and goal poses
+% --- Start and Goal ---
 start = [-2.47543, -3.5765, pi];
 goal = [-13.1368, 2.67684, pi/2];
 
 % Show start and goal positions of robot
-plot(start(1), start(2), 'ro');
-plot(goal(1), goal(2), 'mo');
+plot(start(1), start(2), 'ro', 'MarkerFaceColor', 'r');
+plot(goal(1), goal(2), 'mo', 'MarkerFaceColor', 'm');
 
-% Show start and goal heading angle using a line
+% Show heading lines
 r = 0.5;
-plot([start(1), start(1) + r*cos(start(3))], [start(2), start(2) + r*sin(start(3))], 'r-');
-plot([goal(1), goal(1) + r*cos(goal(3))], [goal(2), goal(2) + r*sin(goal(3))], 'm-');
-hold off;
+plot([start(1), start(1) + r*cos(start(3))], ...
+     [start(2), start(2) + r*sin(start(3))], 'r-');
+plot([goal(1), goal(1) + r*cos(goal(3))], ...
+     [goal(2), goal(2) + r*sin(goal(3))], 'm-');
 
-% Inflate the map for path planning
+% --- Inflate the map for planning ---
 inflatedMap = copy(map);
 inflate(inflatedMap, 0.1);
 
-% Define State Space
+% --- State Space ---
 bounds = [inflatedMap.XWorldLimits; inflatedMap.YWorldLimits; [-pi pi]];
 ss = stateSpaceDubins(bounds);
 ss.MinTurningRadius = 0.4;
 
-% Create Validator
+% --- Validator ---
 stateValidator = validatorOccupancyMap(ss);
 stateValidator.Map = inflatedMap;
 stateValidator.ValidationDistance = 0.05;
 
-% Create Path Planner
+% --- Planner ---
 planner = plannerRRT(ss, stateValidator);
 planner.MaxConnectionDistance = 2.5;
 planner.MaxIterations = 30000;
 planner.GoalReachedFcn = @exampleHelperCheckIfGoal;
 
-% Plan the Path
+% --- Plan Path ---
 rng default;
 [pthObj, solnInfo] = plan(planner, start, goal);
 
-% Shorten Path
+% --- Shorten Path ---
 shortenedPath = shortenpath(pthObj, stateValidator);
 
-% Compute Path Lengths
+% --- Path Lengths ---
 originalLength = pathLength(pthObj);
 shortenedLength = pathLength(shortenedPath);
 
-% Plot Original and Shortened Path
+% --- Second Figure: Path with nodes & stations ---
 figure;
 show(map);
 hold on;
+plot(coords_nodes(:,1), coords_nodes(:,2), 'go', ...
+    'MarkerSize', 6, 'LineWidth', 1.5, 'MarkerFaceColor', 'g');
+plot(coords_stations(:,1), coords_stations(:,2), 'yo', ...
+    'MarkerSize', 6, 'LineWidth', 1.5, 'MarkerFaceColor', 'y');
 
-% Plot search tree
-plot(solnInfo.TreeData(:,1), solnInfo.TreeData(:,2), '.-');
-
-% Interpolate and plot original path
-interpolate(pthObj, 300);
-plot(pthObj.States(:,1), pthObj.States(:,2), 'b-', 'LineWidth', 2);
-
-% Interpolate and plot shortened path
-interpolate(shortenedPath, 300);
-plot(shortenedPath.States(:,1), shortenedPath.States(:,2), 'g-', 'LineWidth', 3);
+% Plot shortened path
+plot(shortenedPath.States(:,1), shortenedPath.States(:,2), ...
+    'g-', 'LineWidth', 3);
 
 % Show start and goal
-plot(start(1), start(2), 'ro');
-plot(goal(1), goal(2), 'mo');
-legend('Search Tree', 'Original Path', 'Shortened Path');
-hold off;
+plot(start(1), start(2), 'ro', 'MarkerFaceColor', 'r');
+plot(goal(1), goal(2), 'mo', 'MarkerFaceColor', 'm');
+
+legend('Nodes (Green)', 'Stations (Yellow)', 'Shortened Path', 'Start', 'Goal');
